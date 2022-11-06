eb9c1a98b37258072133e2295a48776a080f2566
Manoekr <106478859+Manoekr@users.noreply.github.com>
2022-11-06 23:21:55 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'Rentacarr';
+  user:string = "Münir Erkar";
+}
---
+++ src/app/models/rental.ts
@@ -0,0 +1,8 @@
+export interface Rental{
+    rentalId:number;
+    id:number;
+    userId:number;
+    customerId:number;
+    rentDate:number;
+    returnDate:number;
+}
